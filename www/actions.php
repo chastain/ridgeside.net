b6a53dac6093d4220afc08dfead94bd59ef6b61a
Eric Chastain <eric@chastain.me>
2020-07-03 23:17:31 -0400

    Add google analytics

--- www/actions.php
+++ www/actions.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+<?php include 'includes/analytics.php';?>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,75 +20,75 @@
 <h2 class="text-center p-4 heading">Ridgeside Comission Actions</h2>
 <div class="container p-2">
 
-      <ul>
-        <li><a href="assets/commission-actions/MayorNotesSpring2016.pdf" target="_blank">Mayor's Newsletter 2016</a></li>
-        <li><a href="assets/commission-actions/Ridgeside.cht.pdf" target="_blank"> Ridgeside City Charter</a> (with amendments and repeal of 1925 charter)</li>
-        <li><a href="assets/commission-actions/108thLegislators.pdf" target="_blank">108th Tennessee General Assembly</a></li>
-        <li><a href="assets/commission-actions/2015HESRidgeside.pdf" target="_blank">Agreement Between City of Ridgeside and the Humane Educational Society for the Provision of Animal Care Serices</a></li>
-        <li><a href="assets/commission-actions/2008-01_Prop_Ord_Standards.pdf" target="_blank">Ridgeside Standards and Restrictive Covenants</a> (2008) </li>
-        <li><a href="assets/commission-actions/Standards2013.pdf" target="_blank">Ridgeside Standards </a>(2013) </li>
-        <li><a href="assets/commission-actions/Building Codes.pdf" target="_blank">Building Code </a>(Revised 1981)</li>
-        <li><a href="assets/commission-actions/1990RegPlanCommZoning.pdf" target="_blank">Regional Planning Zoning Recommentations</a> (1990)</li>
-        <li><a href="assets/commission-actions/1993ZoneRestrict.pdf" target="_blank">Ridgeside Zoning Ordinance</a> (1993) </li>
-        <li><a href="assets/commission-actions/1993AmendedBldg Code.pdf" target="_blank">Amended Building Code </a>(1993) </li>
-        <li><a href="assets/commission-actions/Revised Debt Policy.pdf" target="_blank">Revised City Debt Policy Ordinance</a> (2012)</li>
-        <li><a href="assets/commission-actions/Animal Control Waste.pdf" target="_blank">Animal Ordinance </a>(2007)</li>
-        <li><a href="assets/commission-actions/ANIMAL CONTROL Ordinance.pdf" target="_blank">Animal Control Ordinance </a>(2013) </li>
-        <li><a href="assets/commission-actions/Excessive Noise and Barking.pdf" target="_blank">Barking Dog Ordinance</a> (2004) </li>
-        <li><a href="assets/commission-actions/CODE OF ETHICS Ordiance 2007-02.pdf" target="_blank">Code of Ethics</a> (2007) </li>
-        <li><a href="assets/commission-actions/Insurance Pool Membership.pdf" target="_blank">City Pool Insurance</a></li>
-        <li><a href="assets/commission-actions/Burning.pdf" target="_blank">Air Pollution Control Ordinance</a> (2004) </li>
-        <li><a href="assets/commission-actions/Sexual Harrassment Policy 2007-01.pdf" target="_blank">Sexual Harassment Policy</a> (2007)</li>
-        <li><a href="assets/commission-actions/2009RidgesideSurveyResults.pdf" target="_blank">Neighborhood Street Closure Survey Results </a>(2009) </li>
-        <li><a href="assets/commission-actions/2009ResultsStreet.pdf" target="_blank">Neighborhood Street Closure Survey Presentation</a> (2009) </li>
-        <li><a href="assets/commission-actions/Ordinance2009-01proposed.pdf" target="_blank">Street Barrier Ordinance</a> (2009) </li>
-        <li><a href="assets/commission-actions/Permanent Barriers.pdf" target="_blank">Permanent Street Barrier Ordinance</a> (2010)</li>
-        <li><a href="assets/commission-actions/SpeedLimit201501.pdf" target="_blank">Ridgeside Speed Limit Ordinance </a>(2015)</li>
-        <li><a href="assets/commission-actions/Storm Water.pdf" target="_blank">Storm Water Ordinance </a>(2005)</li>
-        <li><a href="assets/commission-actions/Burning.pdf" target="_blank">Hamilton County Pollution Control Board Burning Ordinance</a> (2014)</li>
-        <li><a href="assets/commission-actions/2016 Ridgeside Smoking Resolution.pdf" target="_blank">Ridgeside Pool Smoking Resolution </a>(2016) </li>
-        <li><a href="assets/commission-actions/Ridgeside Budget Public Notice July 2016.pdf" target="_blank">Ridgeside Budget 2016 Public Notice</a> (2016)</li>
-        <li><a href="assets/commission-actions/2016-17_Corrected.pdf" target="_blank">Ridgeside Budget 2016 - corrected </a> (2016) </li>
-        <li><a href="assets/commission-actions/Ridgeside GO CON-Special Meeting Notice (7-19-16)TMLforPoolLights (2) (1).pdf" target="_blank">Ridgeside Special Meeting: Pool and Lights</a> (2016)</li>
-        <li><a href="assets/commission-actions/7-12-16 Reg Meeting Minutes and attachments.pdf" title="Minutes" target="_parent">12 July16 Commission Mtg Minutes</a> (2016)</li>
-        <li><a href="assets/commission-actions/7-28-16 Special Meeting - Budget Readings.pdf" title="Budget">07 July 16 Special Budget Mtg Minutes</a> (2016)</li>
-        <li><a href="assets/commission-actions/7-28-16 Special Meeting - Loan Resolution Approval.pdf" title="Approval">07 July 16 Special Commission Mtg, Resolution Approval</a> (2016)</li>
-        <li><a href="assets/commission-actions/10-18-16 Regular Meeting Minutes Scan.pdf" target="_blank">18 Oct 16 City Commission Mtg Minutes </a>(2016)</li>
-        <li><a href="assets/commission-actions/10-18-16 Special Meeting Minutes Scan.pdf" target="_blank">18 Oct 16 Special Meeting Minutes</a> (2016)</li>
-        <li><a href="assets/commission-actions/2017-FinalBudget20152016.pdf" target="_blank">2016 Final Budget 20 15 2016 </a>
-        <li><a href="assets/commission-actions/011717 RegularMtgMin.pdf" target="_blank">17 Jan 17 Regular Meeting Minutes</a> (2017) </li>
-		  <li><a href="assets/commission-actions/2017-0729 StreamRecycling.pdf" target="_blank">2017 Stream Recycling Information</a> (2017)</li>
-		  <li><a href="assets/commission-actions/2017 0530 Signed 1st Budget Readings.pdf" target="_blank">2017 0530 Signed 1st Budget Reading </a>(2017)</li>
-		  <li><a href="assets/commission-actions/2017 061Signed 2nd & 3rd Budget.pdf" target="_blank">2017 Signed 2nd, 3rd Budget Reading</a> (2017)</li>
-		  <li><a href="assets/commission-actions/2017 0418 SignedMinutes.pdf" target="_blank"> 2017 0418 Signed Minutes</a>
-       (2017)</li>
-		  <li><a href="assets/commission-actions/2017 0704 Meeting Minutes.pdf" target="_blank">2017 July Meeting Minutes </a>(2017)</li>
-		  <li><a href="assets/commission-actions/2017 0810 Tax Rate Ordinance.pdf" target="_blank">2017 0810 Tax Rate Proposal</a> (2017)</li>
-		  <li><a href="assets/commission-actions/2017 0829  Tax Rate Ordinanc.pdf" target="_blank"> 2017 0829 Tax Rate Ordinance</a> (2017) </li>
-		  <li><a href="assets/commission-actions/2017 1017 Meet Minutes.pdf" target="_blank"> 2017 1017 Meeting Minutes</a> (2017) </li>
-		  <li><a href="assets/commission-actions/OpenRecords Pol.pdf" target="_blank"> Open Records Policy</a> (2017) </li>
-		  <li><a href="assets/commission-actions/2017 Air Pollute Ord.pdf" target="_blank"> Air Pollution Control Ordinance</a> (2017) </li>
-		  <li><a href="assets/commission-actions/1-25-18 Regular Meeting Minutes.pdf" target="_blank"> 25 Jan 18 Regular Meeting Minutes</a> (2018) </li>
-		  <li><a href="assets/commission-actions/6-18-18 Special Meeting Minutes scan.pdf" target="_blank"> 18 June 18 Special Meeting Minutes</a> (2018)</li>
-		  <li><a href="assets/commission-actions/6-11-18 Special Meeting Minutes scan.pdf" target="_blank"> 11 June 18 Special Meeting Minutes</a> (2018)</li>
-		  <li><a href="assets/commission-actions/4-24-18 Regular Meeting Minutes scan.pdf" target="_blank"> 24 April 18 Regular Meeting Minutes</a> (2018) </li>
-		  <li><a href="assets/commission-actions/7-17-18 RegMeetMin.pdf" target="_blank"> 17 July 18 Regular Meeting Minutes</a> (2018) </li>
-		  <li><a href="assets/commission-actions/Oct2018 QtrMin.pdf" target="_blank"> October 18 Regular Meeting Minutes</a> (2018) </li>
-          <li><a href="assets/commission-actions/15Jan19 Minutes.pdf" target="_blank">15 Jan 19 Regular Meeting Minutes</a> (2019)</li>
-		  <li><a href="assets/commission-actions/16Apr19 Minutes001.pdf" target="_blank">16 April 2019 Regular Meeting Minutes</a>(2019)</li>
-		  <li><a href="assets/commission-actions/18Jun19-Budget-Minutes001.pdf" target="_blank">18-25 June 2019 Budget Meeting Minutes</a>(2019)</li>
-		  <li><a href="assets/commission-actions/7-16-19RegMtgMin.pdf" target="_blank">16 July 2019 Regular Meeting Minutes</a>(2019) </li>
-		  <p></p>
-        <p></p>
-        <li><strong>TOWN MEETINGS</strong> Regular town meetings are held on the third Tuesday of January, April, July, and October. Meetings are held at the Greater Emmanuel Apostolic Church at 400 Tunnel Boulevard and begin at 7:00 pm. Notices of these meetings are printed in the <em>Chattanooga Times / Free Press</em>, posted on this website, and publicized by email. </li>
-        <p></p>
-        <li><strong>VOTING PLACE</strong> The pool office is used on all regular voting days. Please register and vote in Ridgeside. Voting days are announced in the <em>Chattanooga Times/Free Press</em>. </li>
-        <p></p>
-        <li><strong>TENNESSEE INCOME TAXES</strong> Ridgeside receives a share of Tennessee Income taxes paid on interest from investments, etc. The code 334 should be used when filing your Tennessee State Income Tax Form (especially significant for new residents or first time filers). </li>
-        <p></p>
-        <li><strong>STREET PARKING</strong> Please do not park your car on the street. Cars parked on the street obstruct vision, detract from the beauty of our neighborhood, and are a traffic hazard/danger to children. Street parked cars also attract nefarious non-resident villans intent upon easy pilfering. Please park in your driveway or garage. </li>
-        <p></p>
-      </ul>
+  <ul>
+    <li><a href="assets/commission-actions/MayorNotesSpring2016.pdf" target="_blank">Mayor's Newsletter 2016</a></li>
+    <li><a href="assets/commission-actions/Ridgeside.cht.pdf" target="_blank"> Ridgeside City Charter</a> (with amendments and repeal of 1925 charter)</li>
+    <li><a href="assets/commission-actions/108thLegislators.pdf" target="_blank">108th Tennessee General Assembly</a></li>
+    <li><a href="assets/commission-actions/2015HESRidgeside.pdf" target="_blank">Agreement Between City of Ridgeside and the Humane Educational Society for the Provision of Animal Care Serices</a></li>
+    <li><a href="assets/commission-actions/2008-01_Prop_Ord_Standards.pdf" target="_blank">Ridgeside Standards and Restrictive Covenants</a> (2008) </li>
+    <li><a href="assets/commission-actions/Standards2013.pdf" target="_blank">Ridgeside Standards </a>(2013) </li>
+    <li><a href="assets/commission-actions/Building Codes.pdf" target="_blank">Building Code </a>(Revised 1981)</li>
+    <li><a href="assets/commission-actions/1990RegPlanCommZoning.pdf" target="_blank">Regional Planning Zoning Recommentations</a> (1990)</li>
+    <li><a href="assets/commission-actions/1993ZoneRestrict.pdf" target="_blank">Ridgeside Zoning Ordinance</a> (1993) </li>
+    <li><a href="assets/commission-actions/1993AmendedBldg Code.pdf" target="_blank">Amended Building Code </a>(1993) </li>
+    <li><a href="assets/commission-actions/Revised Debt Policy.pdf" target="_blank">Revised City Debt Policy Ordinance</a> (2012)</li>
+    <li><a href="assets/commission-actions/Animal Control Waste.pdf" target="_blank">Animal Ordinance </a>(2007)</li>
+    <li><a href="assets/commission-actions/ANIMAL CONTROL Ordinance.pdf" target="_blank">Animal Control Ordinance </a>(2013) </li>
+    <li><a href="assets/commission-actions/Excessive Noise and Barking.pdf" target="_blank">Barking Dog Ordinance</a> (2004) </li>
+    <li><a href="assets/commission-actions/CODE OF ETHICS Ordiance 2007-02.pdf" target="_blank">Code of Ethics</a> (2007) </li>
+    <li><a href="assets/commission-actions/Insurance Pool Membership.pdf" target="_blank">City Pool Insurance</a></li>
+    <li><a href="assets/commission-actions/Burning.pdf" target="_blank">Air Pollution Control Ordinance</a> (2004) </li>
+    <li><a href="assets/commission-actions/Sexual Harrassment Policy 2007-01.pdf" target="_blank">Sexual Harassment Policy</a> (2007)</li>
+    <li><a href="assets/commission-actions/2009RidgesideSurveyResults.pdf" target="_blank">Neighborhood Street Closure Survey Results </a>(2009) </li>
+    <li><a href="assets/commission-actions/2009ResultsStreet.pdf" target="_blank">Neighborhood Street Closure Survey Presentation</a> (2009) </li>
+    <li><a href="assets/commission-actions/Ordinance2009-01proposed.pdf" target="_blank">Street Barrier Ordinance</a> (2009) </li>
+    <li><a href="assets/commission-actions/Permanent Barriers.pdf" target="_blank">Permanent Street Barrier Ordinance</a> (2010)</li>
+    <li><a href="assets/commission-actions/SpeedLimit201501.pdf" target="_blank">Ridgeside Speed Limit Ordinance </a>(2015)</li>
+    <li><a href="assets/commission-actions/Storm Water.pdf" target="_blank">Storm Water Ordinance </a>(2005)</li>
+    <li><a href="assets/commission-actions/Burning.pdf" target="_blank">Hamilton County Pollution Control Board Burning Ordinance</a> (2014)</li>
+    <li><a href="assets/commission-actions/2016 Ridgeside Smoking Resolution.pdf" target="_blank">Ridgeside Pool Smoking Resolution </a>(2016) </li>
+    <li><a href="assets/commission-actions/Ridgeside Budget Public Notice July 2016.pdf" target="_blank">Ridgeside Budget 2016 Public Notice</a> (2016)</li>
+    <li><a href="assets/commission-actions/2016-17_Corrected.pdf" target="_blank">Ridgeside Budget 2016 - corrected </a> (2016) </li>
+    <li><a href="assets/commission-actions/Ridgeside GO CON-Special Meeting Notice (7-19-16)TMLforPoolLights (2) (1).pdf" target="_blank">Ridgeside Special Meeting: Pool and Lights</a> (2016)</li>
+    <li><a href="assets/commission-actions/7-12-16 Reg Meeting Minutes and attachments.pdf" title="Minutes" target="_parent">12 July16 Commission Mtg Minutes</a> (2016)</li>
+    <li><a href="assets/commission-actions/7-28-16 Special Meeting - Budget Readings.pdf" title="Budget">07 July 16 Special Budget Mtg Minutes</a> (2016)</li>
+    <li><a href="assets/commission-actions/7-28-16 Special Meeting - Loan Resolution Approval.pdf" title="Approval">07 July 16 Special Commission Mtg, Resolution Approval</a> (2016)</li>
+    <li><a href="assets/commission-actions/10-18-16 Regular Meeting Minutes Scan.pdf" target="_blank">18 Oct 16 City Commission Mtg Minutes </a>(2016)</li>
+    <li><a href="assets/commission-actions/10-18-16 Special Meeting Minutes Scan.pdf" target="_blank">18 Oct 16 Special Meeting Minutes</a> (2016)</li>
+    <li><a href="assets/commission-actions/2017-FinalBudget20152016.pdf" target="_blank">2016 Final Budget 20 15 2016 </a>
+    <li><a href="assets/commission-actions/011717 RegularMtgMin.pdf" target="_blank">17 Jan 17 Regular Meeting Minutes</a> (2017) </li>
+    <li><a href="assets/commission-actions/2017-0729 StreamRecycling.pdf" target="_blank">2017 Stream Recycling Information</a> (2017)</li>
+    <li><a href="assets/commission-actions/2017 0530 Signed 1st Budget Readings.pdf" target="_blank">2017 0530 Signed 1st Budget Reading </a>(2017)</li>
+    <li><a href="assets/commission-actions/2017 061Signed 2nd & 3rd Budget.pdf" target="_blank">2017 Signed 2nd, 3rd Budget Reading</a> (2017)</li>
+    <li><a href="assets/commission-actions/2017 0418 SignedMinutes.pdf" target="_blank"> 2017 0418 Signed Minutes</a> (2017)</li>
+    <li><a href="assets/commission-actions/2017 0704 Meeting Minutes.pdf" target="_blank">2017 July Meeting Minutes </a>(2017)</li>
+    <li><a href="assets/commission-actions/2017 0810 Tax Rate Ordinance.pdf" target="_blank">2017 0810 Tax Rate Proposal</a> (2017)</li>
+    <li><a href="assets/commission-actions/2017 0829  Tax Rate Ordinanc.pdf" target="_blank"> 2017 0829 Tax Rate Ordinance</a> (2017) </li>
+    <li><a href="assets/commission-actions/2017 1017 Meet Minutes.pdf" target="_blank"> 2017 1017 Meeting Minutes</a> (2017) </li>
+    <li><a href="assets/commission-actions/OpenRecords Pol.pdf" target="_blank"> Open Records Policy</a> (2017) </li>
+    <li><a href="assets/commission-actions/2017 Air Pollute Ord.pdf" target="_blank"> Air Pollution Control Ordinance</a> (2017) </li>
+    <li><a href="assets/commission-actions/1-25-18 Regular Meeting Minutes.pdf" target="_blank"> 25 Jan 18 Regular Meeting Minutes</a> (2018) </li>
+    <li><a href="assets/commission-actions/6-18-18 Special Meeting Minutes scan.pdf" target="_blank"> 18 June 18 Special Meeting Minutes</a> (2018)</li>
+    <li><a href="assets/commission-actions/6-11-18 Special Meeting Minutes scan.pdf" target="_blank"> 11 June 18 Special Meeting Minutes</a> (2018)</li>
+    <li><a href="assets/commission-actions/4-24-18 Regular Meeting Minutes scan.pdf" target="_blank"> 24 April 18 Regular Meeting Minutes</a> (2018) </li>
+    <li><a href="assets/commission-actions/7-17-18 RegMeetMin.pdf" target="_blank"> 17 July 18 Regular Meeting Minutes</a> (2018) </li>
+    <li><a href="assets/commission-actions/Oct2018 QtrMin.pdf" target="_blank"> October 18 Regular Meeting Minutes</a> (2018) </li>
+    <li><a href="assets/commission-actions/15Jan19 Minutes.pdf" target="_blank">15 Jan 19 Regular Meeting Minutes</a> (2019)</li>
+    <li><a href="assets/commission-actions/16Apr19 Minutes001.pdf" target="_blank">16 April 2019 Regular Meeting Minutes</a>(2019)</li>
+    <li><a href="assets/commission-actions/18Jun19-Budget-Minutes001.pdf" target="_blank">18-25 June 2019 Budget Meeting Minutes</a>(2019)</li>
+    <li><a href="assets/commission-actions/7-16-19RegMtgMin.pdf" target="_blank">16 July 2019 Regular Meeting Minutes</a>(2019) </li>
+    </ul>
+
+    <p></p>
+      <p></p>
+      <li><strong>TOWN MEETINGS</strong> Regular town meetings are held on the third Tuesday of January, April, July, and October. Meetings are held at the Greater Emmanuel Apostolic Church at 400 Tunnel Boulevard and begin at 7:00 pm. Notices of these meetings are printed in the <em>Chattanooga Times / Free Press</em>, posted on this website, and publicized by email. </li>
+      <p></p>
+      <li><strong>VOTING PLACE</strong> The pool office is used on all regular voting days. Please register and vote in Ridgeside. Voting days are announced in the <em>Chattanooga Times/Free Press</em>. </li>
+      <p></p>
+      <li><strong>TENNESSEE INCOME TAXES</strong> Ridgeside receives a share of Tennessee Income taxes paid on interest from investments, etc. The code 334 should be used when filing your Tennessee State Income Tax Form (especially significant for new residents or first time filers). </li>
+      <p></p>
+      <li><strong>STREET PARKING</strong> Please do not park your car on the street. Cars parked on the street obstruct vision, detract from the beauty of our neighborhood, and are a traffic hazard/danger to children. Street parked cars also attract nefarious non-resident villans intent upon easy pilfering. Please park in your driveway or garage. </li>
+      <p></p>
 
     <div class="container">
       <div class="row">
